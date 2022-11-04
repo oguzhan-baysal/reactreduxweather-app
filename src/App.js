40367cac781980f498c044a6fe03ba54d26a933c
Oguzhan <oguzhanbaysal@outlook.com>
2022-11-04 16:23:57 +0300

    react weatherApp is created

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import Cities from "./components/cities";
+import Weather from "./components/weather";
+import { useSelector } from "react-redux";
+import Footer from "./components/footer";
 
 function App() {
+  const items = useSelector((state) => state.weather.items);
+  let weatherMain;
+  let background;
+
+  if (items) {
+    weatherMain = items.current.weather[0].main;
+    console.log(weatherMain);
+    if (weatherMain === "Haze" || weatherMain === "Mist") {
+      weatherMain = "Fog";
+    } else if (weatherMain === "Thunderstorm" || weatherMain === "Thunder") {
+      weatherMain = "Rain";
+    } else if (
+      weatherMain !== "Clouds" &&
+      weatherMain !== "Clear" &&
+      weatherMain !== "Fog" &&
+      weatherMain !== "Rain" &&
+      weatherMain !== "Snow"
+    ) {
+      weatherMain = "default";
+    }
+    background = `/images/${weatherMain}.jpg`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+      <Cities />
+      <Weather />
+      <Footer />
     </div>
   );
 }
